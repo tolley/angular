8a6e2fb1cc84a93bd1fe74128ec6fbad5dff1499
tolley <chris.tolley@gmail.com>
2014-02-25 23:32:42 -0500

    Started playing around with angularjs

---
+++ index.php
@@ -0,0 +1,27 @@
+<?PHP
+// A place for me to learn angularjs
+
+?><doctype !html>
+<html ng-app>
+
+<head>
+
+<script type="text/javascript" src="/js/jquery.js"></script>
+<script type="text/javascript" src="/js/angular.js"></script>
+
+<script type="text/javascript">
+
+$( document ).ready( function()
+{
+	alert( 'ready for javascript' );
+} );
+
+</script>
+
+</head>
+
+<body>
+Hello World!
+</body>
+
+</html>
